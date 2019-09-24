af91f38c9ecb0eda28e0a5a50710a0925596e7b1
Kajetan Terlecki <k.terlecki@wp.pl>
2019-09-24 17:02:53 +0200

    ADD class Product and prepare renderInMenu shema

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,46 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){; //argumenty dla funkcji constructora
+      const thisProduct = this;
+      thisProduct.id = id; //nadanie wartości
+      thisProduct.data = data; //nadanie wartości
+
+      thisProduct.renderInMenu(); //wykonanie metody renderInMenu
+
+      console.log('thisProduct: ', thisProduct);
+    }
+    renderInMenu(){ //metoda tworząca/renderująca m.in nowy kod na stronie
+      const thisProduct = this;
+
+      //Generate HTML based on template
+
+      //create element using utils.createElementFromHTML
+
+      //find menu container
+
+      //add element to menu
+
+    }
+  }
+
   const app = {
+    initMenu: function (){
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data); //=dataSource - thisApp pobiera dane z pliku data.js
+      for(let productData in thisApp.data.products){  //pętla iterująca po products w zewnętrznym pliku data
+        new Product(productData, thisApp.data.products[productData]); //dodanie instancji dla każdego produktu wraz z argumentami
+      }
+
+    },
+
+    initData: function(){   // initData - metoda do pobierania danych z innych źródeł
+      const thisApp = this;
+
+      thisApp.data = dataSource; // dataSource - stała zadeklarowana w pliku src/js/data.js w której są produkty na pizze
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +99,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData(); //wykonanie metody initData
+      thisApp.initMenu(); //wykonanie metody initMenu
     },
   };
 
